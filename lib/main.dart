cf2f7604efe537301aeda3e34a1a334f41fba73d
Aditya <82517731+aditya002ai@users.noreply.github.com>
2021-06-11 21:08:13 +0530

    json load data

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Json Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Json Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Center(
+          child: FutureBuilder(
+            future: DefaultAssetBundle.of(context)
+                .loadString('load_json/person.json'),
+            builder: (context, snapshot) {
+              //Decode the json file
+              var mydata = json.decode(snapshot.data.toString());
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text('Name: ' + mydata[index]['name']),
+                        Text('Age: ' + mydata[index]['age']),
+                        Text('Gender: ' + mydata[index]['gender']),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: mydata == null ? 0 : mydata.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
